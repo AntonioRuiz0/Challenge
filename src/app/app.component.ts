b3bc30b140d63634b2fddd2c2a31c3819720cbed
AntonioRuiz0 <68989871+AntonioRuiz0@users.noreply.github.com>
2020-07-30 01:18:13 -0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'challenge';
+  username = 'Antonio Ruiz';
+  
+
+  onpress() {
+   var title = (<HTMLInputElement>document.getElementById('blogtitle')).value;    
+  console.log(title);
+  document.getElementById('titleblog').innerHTML = title;
+
+  var content = (<HTMLInputElement>document.getElementById('contentblog')).value;
+  console.log(content);
+  document.getElementById('blogcontent').innerHTML = content;
+
+  
+  }
 }
